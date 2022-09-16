6826f33bf9eaa4def53bf3e7e0cceba20be2d4b8
Robert Rupf <robert.rupf@yakamara.de>
2022-09-16 11:15:53 +0200

    base template

--- redaxo/data/addons/developer/templates/Default [1]/template.php
+++ redaxo/data/addons/developer/templates/Default [1]/template.php
@@ -1,1 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>YDeploy-Demo</title>
+</head>
+<body>
+<h1>YDeploy-Demo</h1>
 REX_ARTICLE[]
+</body>
+</html>
